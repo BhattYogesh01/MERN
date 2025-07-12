a93c0c9b8670947916b0911a1329a11ebbd70f67
Yogesh Bhatt <141019052+BhattYogesh01@users.noreply.github.com>
2025-07-12 22:33:05 +0545

    Delete BackEnd directory

--- BackEnd/database/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const ConnectString = "mongodb+srv://yogesh:yogesh@cluster0.2ptxy1b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-async function connectToDatabase(){
-   await mongoose.connect(ConnectString)
-   console.log("connected to DB Successfully")
-}
-
-module.exports = connectToDatabase
